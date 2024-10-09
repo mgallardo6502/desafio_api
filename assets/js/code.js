2c8caa375fb271dc4fee8b438e662893a4795730
Marcelo Gallardo <marcelo.gallardo.6502@gmail.com>
2024-10-09 15:42:08 -0300

    Segundo commit

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,19 +1,33 @@
 // https://mindicador.cl/api/{tipo_indicador}
 // uf, ivp, dolar, dolar_intercambio, euro, ipc, utm, imacec, tpm, libra_cobre, tasa_desempleo, bitcoin
+const btnConvert = document.getElementById("convert");
+const resultSpan = document.getElementById("result");
+const apiURL = "https://mindicador.cl/api/";
 
-const clpValue = document.getElementById("clpInput").value
-const currencySelect = document.getElementById("currency").value
-const btnConvert = document.getElementById("convert")
-const resultSpan = document.getElementById("result")
-const apiURL = "https://mindicador.cl/api/"
+btnConvert.addEventListener("click", function () {
+  renderCurrency();
+});
 
-async function getCurrency() {
+async function getCurrency(currency) {
   try {
-    const res = await fetch(apiURL)
-    const currencyData = await res.json()
-  } catch(err) {
-    alert("Bruh")
+    const res = await fetch(apiURL + currency);
+    const currencyData = await res.json();
+    return currencyData.serie[0].valor
+  } catch (err) {
+    alert(err.message);
   }
 }
 
-getCurrency()
+async function renderCurrency() {
+  const clpValue = document.getElementById("clpInput").value;
+  const currencyOption = document.getElementById("currency").value;
+  const todayValue = await getCurrency(currencyOption);
+  if (clpValue <= 0) {
+    resultSpan.innerHTML = "El número ingresado debe ser mayor a 0";
+  } else if (clpValue > 0) {
+    const exchangeRate = 1 / todayValue
+    resultSpan.innerHTML = `Resultado: ${clpValue * exchangeRate}`
+  } else {
+    resultSpan.innerHTML = "Debes ingresar un número"
+  }
+}
